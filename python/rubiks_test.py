e03e03a0d4c56a9f973e6695675ce19ac3992467
Daniel Walton <dwalton76@gmail.com>
2015-09-26 09:58:55 -0400

    Log all output to /tmp/rubiks.log

--- python/rubiks_test.py
+++ python/rubiks_test.py
@@ -6,33 +6,38 @@ from time import time as current_time
 import logging
 import sys
 
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(filename='/tmp/rubiks.log',
+                    level=logging.INFO,
                     format='%(asctime)s %(filename)12s %(levelname)8s: %(message)s')
 log = logging.getLogger(__name__)
 log.info('import complete')
 
-rub = Rubiks()
+try:
+    rub = Rubiks()
 
-while True:
-    rub.wait_for_cube_insert()
-    rub.scan()
+    while True:
+        rub.wait_for_cube_insert()
+        rub.scan()
 
-    if rub.shutdown_flag:
-        break
+        if rub.shutdown_flag:
+            break
 
-    last_time = current_time()
-    rub.resolve(True)
+        last_time = current_time()
+        rub.resolve(True)
 
-    if rub.shutdown_flag:
-        break
+        if rub.shutdown_flag:
+            break
 
-    total = int(current_time() - last_time)
-    rub.talk(str(total) + " seconds. Ha ha ha.")
+        total = int(current_time() - last_time)
+        rub.talk(str(total) + " seconds. Ha ha ha.")
 
-    rub.mot_push.wait_for_stop()
-    rub.mot_bras.wait_for_stop()
-    rub.mot_rotate.wait_for_stop()
-    rub.mot_push.stop()
-    rub.mot_bras.stop()
-    rub.mot_rotate.stop()
-    rub.wait_for_cube_removal()
+        rub.mot_push.wait_for_stop()
+        rub.mot_bras.wait_for_stop()
+        rub.mot_rotate.wait_for_stop()
+        rub.mot_push.stop()
+        rub.mot_bras.stop()
+        rub.mot_rotate.stop()
+        rub.wait_for_cube_removal()
+except Exception as e:
+    log.exception(e)
+    sys.exit(1)
